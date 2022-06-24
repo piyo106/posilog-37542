27dd17ac457aa2b5165f5d7f0ee144805fedfe04
piyo106 <wolkige.mond@gmail.com>
2022-06-24 15:46:25 +0900

    Logモデルの単体テスト記述

--- spec/factories/logs.rb
+++ spec/factories/logs.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :log do
-    
+    content {Faker::Lorem.sentence}
+    category_id { Faker::Number.between(from: 2, to: 5) }
+    association :goal
   end
 end
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @log = FactoryBot.build(:log)
+  end
+
+  describe 'OODA記録' do
+    context 'OODA記録が保存できる場合' do
+      it 'OODA選択し、内容が存在していれば保存できる' do
+        expect(@log).to be_valid
+      end
+    end
+
+    context 'OODA記録が保存できない場合' do
+      it 'OODAが初期値だと保存できない' do
+        @log.category_id = 1
+        @log.valid?
+        expect(@log.errors.full_messages).to include "OODAを選択してください"
+      end
+      it '内容が空だと保存できない' do
+        @log.content = ''
+        @log.valid?
+        expect(@log.errors.full_messages).to include "内容を入力してください"
+      end
+      it 'goalが紐づいていないと保存できない' do
+        @log.goal = nil
+        @log.valid?
+        expect(@log.errors.full_messages).to include "Goalを入力してください"
+      end
+    end
+  end
 end
