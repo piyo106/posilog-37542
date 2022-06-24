617ffce69c36afb04f7342fe51c248d2e991f183
piyo106 <wolkige.mond@gmail.com>
2022-06-24 23:23:44 +0900

    OODA記録の一覧表示機能実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,14 +2,16 @@ class LogsController < ApplicationController
   def index
     @log = Log.new
     @goal = Goal.find(params[:goal_id])
+    @logs = @goal.logs
   end
-
+  
   def create
     @goal = Goal.find(params[:goal_id])
     @log = @goal.logs.new(log_params)
     if @log.save
       redirect_to  goal_logs_path(@goal)
     else
+      # @logs = @goal.logs
       render :index
     end
   end
